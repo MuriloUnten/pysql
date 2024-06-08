16e943f366d8f930d887f02274a53b138f173496
Murilo Unten <murilo.unten@proton.me>
2024-06-08 15:50:47 -0300

    Add table and column classes

---
+++ backend/Table.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class Table:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.pk = self.getPrimaryKey()
+
+    def getPrimaryKey(self):
+        for col in self.cols:
+            if col.key == "PRI":
+                return col.name
+
+        return "No Primary Key"
