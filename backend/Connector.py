7e4124661052ea39df0bc9d089f847c8d1e9ef62
Murilo Unten <murilo.unten@proton.me>
2024-06-08 11:42:56 -0300

    add postgres connector

--- backend/Connector.py
+++ backend/Connector.py
@@ -1,4 +1,5 @@
 import mysql.connector as mysql
+import psycopg2 as postgres
 
 
 class MySQLConnector:
@@ -17,10 +18,28 @@ class MySQLConnector:
         except:
             print(f"error connecting to database {database}. Please try again.")
 
-    def getDBInfo():
+    def getDBInfo(self):
         pass
 
-    def getTableInfo(table):
+    def getTableInfo(self, table):
         pass
 
 
+class PostgresConnector:
+    def __init__(self):
+        pass
+
+    def connect(self, database, user, password, port=5432):
+        self.connection = postgres.connect(
+            database=database,
+            user=user,
+            password=password,
+            port=port,
+        )
+        self.cursor = self.connection.cursor()
+
+    def getDBInfo(self):
+        pass
+
+    def getTableInfo(self, table):
+        pass
