3dedc916c4826a30c140946232e38379528ddf88
IanMSIshikawa <ianishikawa1@gmail.com>
2024-06-08 09:28:53 -0300

    iniciando tkinter

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-
+from tkinter import *
 
 def main():
-    print("Hello World")
+    window = Tk()
+    window.title = "PySQLui"
+    window.geometry('720x720')
+    initialForm(window)
+    window.mainloop()
 
+def initialForm(window):
+    lbl1 = Label(window, text="Nome do banco de dados:")
+    lbl1.grid(column=0, row=0)
+    entry_db = Entry(window, width=30)
+    entry_db.grid(column=1, row=0, padx=10, pady=5)
+
+
+    lbl2 = Label(window, text="Senha do banco de dados:")
+    lbl2.grid(column=0, row=1)
+    entry_password = Entry(window, width=30, show="*")
+    entry_password.grid(column=1, row=1, padx=10, pady=5)
 
 if __name__ == "__main__":
     main()
